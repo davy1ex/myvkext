b2576095a4c3ee9390bac0a73f725b4c829271f3
davy1ex <dezy1ex@gmail.com>
2020-02-22 00:23:30 +0400

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+//console.log("tt")
+var elem = document.getElementById("l_msg");
+
+elem.parentElement.removeChild(elem);
+console.log('gg');
+// var attribute = element.getAttribute(attributeName);
+// <li id="l_msg" class="">
